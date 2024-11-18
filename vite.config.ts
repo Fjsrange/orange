319ccd78bfc7d3c6a9950e5e37c3a72a7f7daba9
unknown <3434782480@qq.com>
2024-11-18 10:45:52 +0800

    修改打包路径和输出目录

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { fileURLToPath } from 'node:url';
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
