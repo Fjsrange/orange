5f720d548606d9667f568cc3b09954d3f6d4906b
unknown <3434782480@qq.com>
2024-11-21 17:30:34 +0800

    duyidao的excel上传预览功能

--- src/views/resource/index.vue
+++ src/views/resource/index.vue
@@ -1,7 +1,64 @@
-<script setup lang="ts"></script>
+<script>
+import { ref } from "vue";
+import vueOfficeExcel from "@vue-office/excel";
+import myUpload from "../../components/myUpload/index.vue";
+import "@vue-office/excel/lib/index.css";
+
+export default {
+  components: {
+    vueOfficeExcel,
+    myUpload,
+  },
+  setup() {
+    const file = ref(null);
+
+    const onChangeFn = (e) => {
+      file.value = e; // 读取文件数据
+    };
+
+    return {
+      file,
+      onChangeFn,
+    };
+  },
+};
+</script>
 
 <template>
-  <div>resource</div>
+  <div class="xlsx">
+    <div class="word-upload">
+      <myUpload needList @change="onChangeFn" />
+    </div>
+    <vueOfficeExcel v-if="file" :src="file" style="height: 500px" />
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.xlsx {
+  margin-top: 15px;
+
+  .word-upload {
+    width: 300px;
+    margin: 0 auto 20px;
+  }
+
+  table {
+    margin-top: 15px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .xlsx {
+    margin-top: 0.9375rem;
+
+    .word-upload {
+      width: 18.75rem;
+      margin-bottom: 1.25rem;
+    }
+
+    table {
+      margin-top: 0.9375rem;
+    }
+  }
+}
+</style>
